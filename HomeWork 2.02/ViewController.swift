4b9f6302fd117ac93dafbb3e3d588c3afa391ff5
AlexeyNovoseltsev <113413099+AlexeyNovoseltsev@users.noreply.github.com>
2022-09-18 00:05:47 +0700

    TrafficLights

--- HomeWork 2.02/ViewController.swift
+++ HomeWork 2.02/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        actionButton.layer.cornerRadius = 10
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        //super.viewWillLayoutSubviews()
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+    @IBAction func settingLight() {
+        
+        actionButton.setTitle("Next", for: .normal)
+        actionButton.backgroundColor = UIColor.purple
+        
+        if redLight.alpha < 1.0 && yellowLight.alpha < 1.0 && greenLight.alpha < 1.0 {
+            redLight.alpha = 1
+        } else if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1.0
+        } else if yellowLight.alpha == 1.0 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1.0
+        } else if greenLight.alpha == 1.0 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1.0
+        }
+        
+    }
+    
 }
+
 
