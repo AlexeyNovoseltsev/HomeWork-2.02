40521d743d681cd96de37273af9b5b38d0b66f76
AlexeyNovoseltsev <113413099+AlexeyNovoseltsev@users.noreply.github.com>
2022-09-20 19:26:22 +0700

    Issues fixed

--- HomeWork 2.02/ViewController.swift
+++ HomeWork 2.02/ViewController.swift
@@ -2,13 +2,12 @@
 //  ViewController.swift
 //  HomeWork 2.02
 //
-//  Created by Alex Friend on 16.09.2022.
+//  Created by Alex Novoseltsev on 16.09.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
     
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
@@ -16,12 +15,17 @@ class ViewController: UIViewController {
     
     @IBOutlet var actionButton: UIButton!
     
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.alpha = 0.3
-        yellowLight.alpha = 0.3
-        greenLight.alpha = 0.3
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
         actionButton.layer.cornerRadius = 10
     }
     
@@ -34,24 +38,46 @@ class ViewController: UIViewController {
     
     @IBAction func settingLight() {
         
-        actionButton.setTitle("Next", for: .normal)
-        actionButton.backgroundColor = UIColor.purple
-        
-        if redLight.alpha < 1.0 && yellowLight.alpha < 1.0 && greenLight.alpha < 1.0 {
-            redLight.alpha = 1
-        } else if redLight.alpha == 1 {
-            redLight.alpha = 0.3
-            yellowLight.alpha = 1.0
-        } else if yellowLight.alpha == 1.0 {
-            yellowLight.alpha = 0.3
-            greenLight.alpha = 1.0
-        } else if greenLight.alpha == 1.0 {
-            greenLight.alpha = 0.3
-            redLight.alpha = 1.0
+        if actionButton.currentTitle == "Start" {
+            actionButton.setTitle("Next", for: .normal)
+            actionButton.backgroundColor = UIColor.purple
         }
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
+        
+//        if redLight.alpha < 1.0 && yellowLight.alpha < 1.0 && greenLight.alpha < 1.0 {
+//            redLight.alpha = 1
+//        } else if redLight.alpha == 1 {
+//            redLight.alpha = 0.3
+//            yellowLight.alpha = 1.0
+//        } else if yellowLight.alpha == 1.0 {
+//            yellowLight.alpha = 0.3
+//            greenLight.alpha = 1.0
+//        } else if greenLight.alpha == 1.0 {
+//            greenLight.alpha = 0.3
+//            redLight.alpha = 1.0
+//        }
         
     }
     
+}
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
 }
 
 
